c86373bd7827ee2fc97997dc54ecd1a96dd8cdb2
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-05-03 10:07:29 +0200

    Update form input validation

--- scripts.js
+++ scripts.js
@@ -5,5 +5,23 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+
+  //Dividing numbers result in a decimal number
+  if (dividend % divider !== 0) {
+    result.innerText = "Error: Division results in a decimal number.";
+    return;
+  };
+
+  //Validation when values are missing
+  if (dividend === "" || divider === "") {
+    result.innerText = "Error: Please fill in all fields.";
+    return;
+  };
+  console.log(dividend, divider);
+  
+
+
   result.innerText = dividend / divider;
 });
+
+// Scenario: Starting program state
